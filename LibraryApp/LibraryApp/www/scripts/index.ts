ca838110ef295100fd374ceb77fa1117b6634bb9
Matthew T. Johnson <matthew.t.johnson@outlook.com>
2017-02-05 20:36:15 -0800

    random

--- LibraryApp/LibraryApp/www/scripts/index.ts
+++ LibraryApp/LibraryApp/www/scripts/index.ts
@@ -15,12 +15,16 @@ module LibraryApp {
             document.addEventListener('pause', onPause, false);
             document.addEventListener('resume', onResume, false);
 
-            // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-            var parentElement = document.getElementById('deviceready');
-            var listeningElement = parentElement.querySelector('.listening');
-            var receivedElement = parentElement.querySelector('.received');
-            listeningElement.setAttribute('style', 'display:none;');
-            receivedElement.setAttribute('style', 'display:block;');
+            try {
+                // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+                var parentElement = document.getElementById('deviceready');
+                var listeningElement = parentElement.querySelector('.listening');
+                var receivedElement = parentElement.querySelector('.received');
+                listeningElement.setAttribute('style', 'display:none;');
+                receivedElement.setAttribute('style', 'display:block;');
+            } catch (e) {
+                // ignore
+            }
         }
 
         function onPause() {
